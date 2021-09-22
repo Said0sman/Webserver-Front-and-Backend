7b293e0383f26bdfc5c0b01f42750549f0588f88
Said Osman <said_henry@hotmail.com>
2021-09-22 08:30:46 +0200

    Added alive function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import {useState} from "react";
+import http from  './utils/api/ProfileApi'
 
 function App() {
-  const [text, setText] = useState('API is Alive')
+  const [text, setText] = useState('API is Alive with Frontend')
+function alive() {
+      http.get('/')
+          .then(function (res){
+              console.log(res)
+          }).catch(function (error){
+              //catch the error
+              console.log(error)
+          return 'Error'
+      }).then(function (){
+          //will always run
+      })
+}
 
     return (
     <diiv>
 <h2>Profile API with Axios</h2>
         <p>{text}</p>
         <button onClick={ () => {setText('New ID')}}>Profile User</button>
+        <button onClick={alive}>Alive</button>
     </diiv>
   );
 }
