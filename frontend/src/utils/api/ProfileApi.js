cdaebec5053986f3109979737059a313b849ee43
Said Osman <said_henry@hotmail.com>
2021-09-22 07:12:50 +0200

    ProfileApi with hostUrl

--- frontend/src/utils/api/ProfileApi.js
+++ frontend/src/utils/api/ProfileApi.js
@@ -1,3 +1,14 @@
 import Axios from 'axios'
 
 
+//const hostUrl = 'http://127.0.0.1'
+const hostUrl = 'http://localhost'
+
+const port = 3001
+const url = hostUrl + ':' + port
+
+const ProfileApi = Axios.create({
+    baseURL: url
+})
+
+export default ProfileApi
