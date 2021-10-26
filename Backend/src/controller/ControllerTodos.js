45b3bd0218784a506b8cded86d92334ddada7aaa
Said Osman <said_henry@hotmail.com>
2021-10-26 17:21:40 +0200

    ControllerTodos is created

--- Backend/src/controller/ControllerTodos.js
+++ Backend/src/controller/ControllerTodos.js
@@ -2,11 +2,25 @@ import ModelTodos from "../models/ModelTodos.js";
 import Logger from "../utils/Logger.js";
 
 
-const createTodos = async (req,res) => {
+const createTodos = async (req, res) => {
     Logger.http(req.body)
 
-}
+    const todos = new ModelTodos({
+        text: req.body.text,
+        day: req.body.day,
+        time: req.body.time,
+    })
+    Logger.debug(todos)
 
+    try {
+        const response = await todos.save()
+        Logger.debug(response)
+        res.status(200).send(response)
+    }
+    catch (error){
+        res.status(500).send({message:error.message})
+    }
+}
 export default {
-    createTodos
+    createTodos,
 }
